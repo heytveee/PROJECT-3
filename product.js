b975de0324c64a5964c8cc3bdcb103ac620dbe41
heytveee <HETVIP243@GMAIL.COM>
2024-07-24 17:45:42 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,20 @@
+
+const products = new Map()
+products.set('item-cart-1', [
+  {
+    full_img_url: 'https://cdn.shopify.com/s/files/1/0651/0847/6121/files/LINEA-SML1_2397x.jpg?v=1691696179',
+    thumb_img_url: 'https://cdn.shopify.com/s/files/1/0651/0847/6121/files/LINEA-SML1_2397x.jpg?v=1691696179',
+  },
+  {
+    full_img_url: 'https://cdn.shopify.com/s/files/1/0651/0847/6121/files/LINEA1_c68b356a-7773-4539-883b-b7c69240702b_2397x.jpg?v=1693397209',
+    thumb_img_url: 'https://cdn.shopify.com/s/files/1/0651/0847/6121/files/LINEA1_c68b356a-7773-4539-883b-b7c69240702b_2397x.jpg?v=1693397209',
+  },
+  {
+    full_img_url: 'https://cdn.shopify.com/s/files/1/0651/0847/6121/files/LINEA5_b8d0d2ce-fd6b-430c-8f0a-7cdf7fdc9bc0_2397x.jpg?v=1693397209',
+    thumb_img_url: 'https://cdn.shopify.com/s/files/1/0651/0847/6121/files/LINEA5_b8d0d2ce-fd6b-430c-8f0a-7cdf7fdc9bc0_2397x.jpg?v=1693397209',
+  },
+  {
+    full_img_url: 'https://cdn.shopify.com/s/files/1/0651/0847/6121/files/LINEA6_18a5d346-6419-4c69-9af2-4af59a08ea2f_2397x.jpg?v=1693397209',
+    thumb_img_url: 'https://cdn.shopify.com/s/files/1/0651/0847/6121/files/LINEA6_18a5d346-6419-4c69-9af2-4af59a08ea2f_2397x.jpg?v=1693397209',
+  }
+])
